a4e0cc68eb0fb0c07c8a39ec0d6118a6a3d7d17f
Jesus Lara <jelara99@gmail.com>
2022-04-20 21:12:22 -0600

    footer y logos

---
+++ components/layouts/footer/footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {};
+
+export default function Footer({}: Props) {
+  return (
+    <footer className="container-fluid">
+      <div className="container pb-3">contactanos</div>
+    </footer>
+  );
+}
